33271c5346d29cc02e0cee589fbf1fe49e484be3
WGand <wgandino@gmail.com>
2022-10-25 18:26:32 -0400

    hot fix?

--- dist/src/Message/Message.d.ts
+++
@@ -1,9 +0,0 @@
-import { Lesson } from 'src/Domain/Lesson';
-import { Student } from '../Student/Student';
-export declare class Message {
-    student: Student;
-    publishedAt: Date;
-    lesson: Lesson;
-    content: string;
-    getContent(): string;
-}
--- dist/src/Student/createStudent.dto.d.ts
+++
@@ -1,4 +0,0 @@
-export declare class createStudentDto {
-    name: string;
-    surname: string;
-}
