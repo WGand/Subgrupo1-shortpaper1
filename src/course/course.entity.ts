5748d18124cd045a553ff87f824387ccb83553bf
WGand <wgandino@gmail.com>
2022-10-25 17:21:19 -0400

    Hot fix

--- src/course/course.entity.ts
+++ src/course/course.entity.ts
@@ -1,33 +1,32 @@
 import {
-    Entity,
-    Column,
-    PrimaryGeneratedColumn,
-    BaseEntity,
-    ManyToMany,
-    JoinTable,
-  } from 'typeorm';
-  import { Student } from 'src/Student/Student.entity';
-  import { CourseSuscriptionStateEnum } from './CourseSubscriptionStateEnum';
-  
-  @Entity('course')
-  export class Course extends BaseEntity {
-    @PrimaryGeneratedColumn()
-    CourseId: number;
-  
-    @Column()
-    title: string;
-  
-    @Column({ array: true })
-    category: string[];
-  
-    @Column( { array: true })
-    Keywords: string[];
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  BaseEntity,
+  ManyToMany,
+  JoinTable,
+} from 'typeorm';
+import { Student } from 'src/Student/student.entity';
+import { CourseSuscriptionStateEnum } from './CourseSubscriptionStateEnum';
 
-    @Column()
-    state: CourseSuscriptionStateEnum;
+@Entity('course')
+export class Course extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  CourseId: number;
 
-    @ManyToMany(() => Student)
-    @JoinTable()
-    Student: Student[];
-  }
-  
+  @Column()
+  title: string;
+
+  @Column({ array: true })
+  category: string[];
+
+  @Column({ array: true })
+  Keywords: string[];
+
+  @Column()
+  state: CourseSuscriptionStateEnum;
+
+  @ManyToMany(() => Student)
+  @JoinTable()
+  Student: Student[];
+}
