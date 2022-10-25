e1baa2ab4ac2968bb24fcc13aa6f20c0f71e3b3f
Aitor <aitorparrilla06@gmail.com>
2022-10-25 15:21:32 -0400

    Tentativo Entity Course

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,10 +6,12 @@ import { AppService } from './app.service';
 import { StudentModule } from './Student/student.module';
 import { MessageService } from './Message/message.service';
 import { MessageController } from './Message/message.controller';
+import { CourseService } from './course/course.service';
+import { CourseController } from './course/course.controller';
 
 @Module({
   imports: [StudentModule, TypeOrmModule.forRoot(typeOrmConfig)],
-  controllers: [AppController, MessageController],
-  providers: [AppService, MessageService],
+  controllers: [AppController, MessageController, CourseController],
+  providers: [AppService, MessageService, CourseService],
 })
 export class AppModule {}
