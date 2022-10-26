b9901b65e7e70c95b07eb07d2d5b85ec7995aff9
Marcos Duque <mjduque.19@est.ucab.edu.ve>
2022-10-25 22:40:29 -0400

    Decorators

---
+++ src/LogDecorator/LogDecorator.controller.ts
@@ -0,0 +1,4 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('log-decorator')
+export class LogDecoratorController {}
--- src/app.module.ts
+++ src/app.module.ts
@@ -6,10 +6,14 @@ import { AppService } from './app.service';
 import { StudentModule } from './Student/student.module';
 import { MessageService } from './Message/message.service';
 import { MessageController } from './Message/message.controller';
+import { MailDecoratorService } from './MailDecorator/MailDecorator.service';
+import { MailDecoratorController } from './MailDecorator/MailDecorator.controller';
+import { LogDecoratorService } from './LogDecorator/LogDecorator.service';
+import { LogDecoratorController } from './LogDecorator/LogDecorator.controller';
 
 @Module({
   imports: [StudentModule, TypeOrmModule.forRoot(typeOrmConfig)],
-  controllers: [AppController, MessageController],
-  providers: [AppService, MessageService],
+  controllers: [AppController, MessageController, MailDecoratorController, LogDecoratorController],
+  providers: [AppService, MessageService, MailDecoratorService, LogDecoratorService],
 })
 export class AppModule {}
