864bfaf934bf41a9f11f1f01dd80ebd5814b7752
Aitor <aitorparrilla06@gmail.com>
2022-10-26 15:38:55 -0400

    FIXES

--- src/course/course.entity.ts
+++ src/course/course.entity.ts
@@ -23,7 +23,7 @@ export class Course extends BaseEntity {
   @Column('text', { array: true, default: {} })
   Keywords: string[];
 
-  @Column( { default: '9' })
+  @Column( { default: '1' })
   state: CourseSuscriptionStateEnum;
 
   @ManyToMany(() => Student, { cascade: true })
