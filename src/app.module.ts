d1cea2381695614ac112622aff0e4b4544454030
WGand <wgandino@gmail.com>
2022-10-26 18:41:48 -0400

    Hot fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,16 +4,10 @@ import { typeOrmConfig } from 'config/config.service';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { StudentModule } from './Student/student.module';
-import { MessageService } from './Message/message.service';
-import { MessageController } from './Message/message.controller';
-import { MailDecoratorService } from './MailDecorator/MailDecorator.service';
-import { MailDecoratorController } from './MailDecorator/MailDecorator.controller';
-import { LogDecoratorService } from './LogDecorator/LogDecorator.service';
-import { LogDecoratorController } from './LogDecorator/LogDecorator.controller';
 
 @Module({
   imports: [StudentModule, TypeOrmModule.forRoot(typeOrmConfig)],
-  controllers: [AppController, MessageController, MailDecoratorController, LogDecoratorController],
-  providers: [AppService, MessageService, MailDecoratorService, LogDecoratorService],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
