657776576377a191d4065156f53832b92df6229d
Aitor <aitorparrilla06@gmail.com>
2022-10-25 22:13:30 -0400

    Lesson Insert

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { StudentModule } from './Student/student.module';
 import { CourseModule } from './course/Course.module';
+import { LessonService } from './lesson/lesson.service';
+import { LessonController } from './lesson/lesson.controller';
 
 @Module({
   imports: [StudentModule, TypeOrmModule.forRoot(typeOrmConfig), CourseModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, LessonController],
+  providers: [AppService, LessonService],
 })
 export class AppModule {}
