066edf9d065f249e05e4ea9596cd7217ffdd0e30
WGand <wgandino@gmail.com>
2022-10-27 01:32:40 -0400

    hot fix

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { LogDecoratorService } from './LogDecorator/LogDecorator.service';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: new LogDecoratorService(), // Creacion de nuestro logger
+  });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   const PORT = process.env.PORT || 3000;
   await app.listen(PORT);
 }
 bootstrap();
-//THIS IS A COMIT TEST
